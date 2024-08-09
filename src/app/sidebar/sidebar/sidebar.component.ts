e9945acd4711cd474fa61291371e3de4208fd4e2
Faten01alsafadi <fatensafad@gmail.com>
2024-08-09 21:35:56 +0300

    Add a navbar and sidebar, download Bootstrap Icons, and link the team's pages with the sidebar.

--- src/app/sidebar/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar/sidebar.component.ts
@@ -7,4 +7,32 @@ import { Component } from '@angular/core';
 })
 export class SidebarComponent {
 
+  constructor() { }
+
+  ngOnInit(): void {
+    const sidebarToggle = document.getElementById('sidebarToggle');
+    const sidebarWrapper = document.getElementById('sidebar-wrapper');
+
+    sidebarToggle?.addEventListener('click', () => {
+      sidebarWrapper?.classList.toggle('active');
+    });
+  }
+ 
+  showFirstDiv = true;
+  showSecondDiv = false;
+
+
+  toggleDivs() {
+    this.showFirstDiv = !this.showFirstDiv;
+    this.showSecondDiv = !this.showSecondDiv;
+  }
+ // حالة لتمثيل التغيير في CSS
+ isChanged = false;
+
+ // دالة لتبديل حالة CSS
+ toggleStyle() {
+   this.isChanged = !this.isChanged;
+ }
+
+
 }
